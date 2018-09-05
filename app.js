c3b51f188cd52c5f9922289308a6dd1d9c240d50
Kirpal <skripal77777@gmail.com>
2018-09-05 19:55:33 +0530

    CHanges

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 var mongoose = require('mongoose');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var products = require('./routes/products');
 var app = express();
 
 // view engine setup
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/products', products);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
